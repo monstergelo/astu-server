ca91f7d0012fe1d730ca5050331b6a183bd8b944
monstergelo <bimawansatrianto@gmail.com>
2022-09-12 14:57:13 +0700

    change env

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 const port = process.env.PORT || 3000
-console.log(port)
+const host = process.env.HOST || "0.0.0.0"
+console.log(port, host)
 
 // Require the framework and instantiate it
 const fastify = Fastify({ logger: true })
@@ -14,7 +15,7 @@ fastify.register(helloRoute);
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: port })
+    await fastify.listen({ port, host })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
