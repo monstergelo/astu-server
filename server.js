df6a4815406e11d4d43f1fa7279f4eb1beeebdbc
monstergelo <bimawansatrianto@gmail.com>
2022-09-12 14:25:21 +0700

    Procfile

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-import helloRoute from './routes/hello'
+import helloRoute from './routes/hello.js'
+import Fastify from 'fastify'
+import dotenv from 'dotenv'
+
+dotenv.config();
+const port = process.env.PORT || 3000
 
 // Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
+const fastify = Fastify({ logger: true })
 
 fastify.register(helloRoute);
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
